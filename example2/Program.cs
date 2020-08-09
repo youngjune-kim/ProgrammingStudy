bd3c559e4143090b0cc6bf894f33501f8c1071d2
youngjune <knmknmkr@naver.com>
2020-08-09 15:09:20 +0900

    Add array example

---
+++ example2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3] { 2, 4, 6 };
+            int b = arr[0]+ arr[1]+ arr[2];
+            Console.WriteLine(b); 
+        }
+    }
+}
