e7c5a6635993eac2e0ebe61976e9b0303fac6102
youngjune-kim <knmknmkr@naver.com>
2020-08-09 14:17:32 +0900

    :sparkles: Add example1

---
+++ example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            float b;
+            float c = 0;
+
+            Console.Write("값을 입력해주세요: ");
+            string str = Console.ReadLine();
+            a = float.Parse(str);
+            Console.Write("값을 입력해주세요: ");
+            str = Console.ReadLine();
+            b = float.Parse(str);
+
+            // 두 변수를 곱하고 소수점을 버린 뒤 출력한다
+            c = a * b;
+            Console.WriteLine((char)c);
+        }
+    }
+}
