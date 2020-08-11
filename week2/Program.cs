5f17b2a4a8e542943af369cf1a04ec66e54b2f94
youngjune <knmknmkr@naver.com>
2020-08-11 21:07:39 +0900

    week2

---
+++ week2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int z;
+            int y;
+
+            Console.WriteLine("a값을 입력해주세요");
+            string str = Console.ReadLine();
+            a = int.Parse(str);
+
+            Console.WriteLine("b값을 입력해주세요");
+            str = Console.ReadLine();
+            b = int.Parse(str);
+
+            Console.WriteLine("c값을 입력해주세요");
+            str = Console.ReadLine();
+            c = int.Parse(str);
+
+            if (a <= b)
+            {
+                z = b;
+            }
+            else
+            {
+                z = a;
+            }
+
+
+            if (c >= z)
+            {
+                y = c;
+                Console.WriteLine(y);
+            }
+            else
+            {
+                Console.WriteLine(z);
+            }
+        }
+    }
+}
+
