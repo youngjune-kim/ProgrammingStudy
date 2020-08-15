e3db8788ef44dbd4b0214b1c1b9bf02ac0cfe0a2
youngjune <knmknmkr@naver.com>
2020-08-15 14:30:57 +0900

    week2-2

---
+++ week2-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace week2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
