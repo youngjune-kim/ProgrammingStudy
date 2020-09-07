9fec7e42892d32b98f3fe78403ddd949c7ca7b2d
youngjune <knmknmkr@naver.com>
2020-09-07 16:30:36 +0900

    클래스 상속성

---
+++ class inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Transactions;
+
+namespace class_inheritance
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+        public Person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+    }
+
+    public class Student : Person
+    {
+        private string college;
+        public Student(int age, string name, string college) : base(age, name)
+        {
+            this.college = college;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// 클래스 상속
