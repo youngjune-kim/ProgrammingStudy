c753f1608ff563c087d1a0cfa9532dc13f5b58df
youngjune <knmknmkr@naver.com>
2020-09-13 16:41:16 +0900

    클래스 정적함수

---
+++ class study2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace class_study2
+{
+    class Program
+    {
+        class YoungMath
+        {
+            public static int Pow(int num, int square)
+            {
+                int result = 1;
+                for (int i = 0; i < square; i++)
+                {
+                    result = num * result;
+                }
+                return result;
+            }
+        }
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("숫자를 입력해주세요");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("제곱근을 입력해주세요");
+            int square = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("결과 값은 {0}입니다.", YoungMath.Pow(num, square));
+        }
+    }
+}
+
+//정적 함수
